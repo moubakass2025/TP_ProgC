ce752483f65e12d00ed678fbe9d2cf6254ea7af5
moubakass2025 <moubarakkassindja@gmail.com>
2025-04-16 15:43:59 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 12;
+    int num2 = 4;
+    char op = '+';  // Change cette valeur pour tester d'autres opérations
+
+    int result; // Pour stocker le résultat (si applicable)
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            printf("%d + %d = %d\n", num1, num2, result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%d - %d = %d\n", num1, num2, result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%d * %d = %d\n", num1, num2, result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("%d / %d = %d\n", num1, num2, result);
+            } else {
+                printf("Erreur : division par zéro.\n");
+            }
+            break;
+        case '%':
+            if (num2 != 0) {
+                result = num1 % num2;
+                printf("%d %% %d = %d\n", num1, num2, result);
+            } else {
+                printf("Erreur : modulo par zéro.\n");
+            }
+            break;
+        case '&':
+            result = num1 & num2;
+            printf("%d & %d = %d\n", num1, num2, result);
+            break;
+        case '|':
+            result = num1 | num2;
+            printf("%d | %d = %d\n", num1, num2, result);
+            break;
+        case '~':
+            result = ~num1;
+            printf("~%d = %d\n", num1, result);
+            break;
+        default:
+            printf("Opérateur non reconnu.\n");
+    }
+
+    return 0;
+}
 
