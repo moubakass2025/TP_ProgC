4c2b3e8ebe01f92a8300ce767df1e7945b291698
moubakass2025 <moubarakkassindja@gmail.com>
2025-04-16 16:45:22 +0200

    Update puissance.c

--- TP2/src/puissance.c
+++ TP2/src/puissance.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int a = 2; // base
-    int b = 3; // exposant
+    int a = 2;      // Base
+    int b = 3;      // Exposant
     int resultat = 1;
 
-    // Cas où l'exposant est négatif (optionnel à gérer selon votre besoin)
-    if (b < 0) {
-        printf("L'exposant ne peut pas être négatif dans ce programme.\n");
-        return 1;
-    }
-
-    // Calcul de la puissance
+    // Boucle pour calculer a^b
     for (int i = 0; i < b; i++) {
         resultat *= a;
     }
 
-    // Affichage du résultat
-    printf("%d élevé à la puissance %d est : %d\n", a, b, resultat);
+    printf("%d à la puissance %d = %d\n", a, b, resultat);
 
     return 0;
 }
